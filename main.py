bc812cc9bddde6edae2df48e1e7b5bee46fe1ba4
pushyanth123 <kpushyanth136@gmail.com>
2022-02-18 17:33:36 +0530

    Added options to specify where to get input from (file (-f) or microphone(-m)).

--- main.py
+++ main.py
@@ -8,26 +8,28 @@ import audio_from_mic
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Script to convert speech to text")
-    parser.add_argument(
-        '-i', '--input', help='Input file (\'-\' To read file name from stdin)', required=False, metavar="")
+        description="Script to convert speech to text", conflict_handler="resolve")
+    inp = parser.add_mutually_exclusive_group(required=False)
+    inp.add_argument(
+        "-i", "--input-file", help='Input from file (\'-\' To read file name from stdin)', action="store",dest = "input_file")
+    inp.add_argument(
+        "-m", "--mic", help='Input from microphone', action="store_true")
+
     parser.add_argument(
         '-o', '--output', help='Output file (\'-\' To output into stdout)', required=False, metavar="")
     parser.add_argument(
         '-f', '--offline', help='Offline mode', required=False, action='store_true')
+
     args = parser.parse_args()
 
-    if args.input == '-':
+    if args.input_file == '-':
         input_file = sys.stdin.readline().strip()
     else:
-        input_file = args.input
+        input_file = args.input_file
 
-    text = ""
-
-    # If "input" argument is not given, read from microphone
     is_quite = False if args.output == '-' or args.output == None else True
 
-    if input_file == None:
+    if args.mic or (args.input_file == None):
         inp = audio_from_mic.FromMicrophone()
         inp.listen(2)
         # Convert speech to text
@@ -41,7 +43,6 @@ def main():
     if args.output != None:
         with open(args.output, 'w') as f:
             f.write(text)
-
 
 
 if __name__ == "__main__":
